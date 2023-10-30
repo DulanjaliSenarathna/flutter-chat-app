cb80d67d50d532160943c2e11bdb60c5641a66d2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-10-30 12:37:45 +0530

    Profile view done

--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -9,11 +9,156 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class ProfilePage extends GetView<ProfileController> {
   const ProfilePage({super.key});
 
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text(
+        "Profile",
+        style: TextStyle(
+            color: AppColors.primaryText,
+            fontSize: 16.sp,
+            fontWeight: FontWeight.normal),
+      ),
+    );
+  }
+
+  Widget _buildProfilePhoto() {
+    return Stack(
+      children: [
+        Container(
+          width: 120.w,
+          height: 120.w,
+          decoration: BoxDecoration(
+              color: AppColors.primarySecondaryBackground,
+              borderRadius: BorderRadius.all(Radius.circular(60.w)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1))
+              ]),
+          child: Image(
+              width: 120.w,
+              height: 120.h,
+              fit: BoxFit.cover,
+              image: const AssetImage("assets/images/account_header.png")),
+        ),
+        Positioned(
+            bottom: 20.w,
+            right: 0.w,
+            height: 35.w,
+            child: GestureDetector(
+              child: Container(
+                height: 35.w,
+                width: 35.w,
+                decoration: BoxDecoration(
+                    color: AppColors.primaryElement,
+                    borderRadius: BorderRadius.all(Radius.circular(40.w))),
+                child: Image.asset("assets/icons/edit.png"),
+              ),
+            ))
+      ],
+    );
+  }
+
+  Widget _buildCompleteBtn() {
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(top: 60.h, bottom: 30.h),
+        decoration: BoxDecoration(
+            color: AppColors.primaryElement,
+            borderRadius: BorderRadius.all(Radius.circular(5.w)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1))
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Complete",
+              style: TextStyle(
+                  color: AppColors.primaryElementText,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.normal),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogOutBtn() {
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(top: 60.h, bottom: 30.h),
+        decoration: BoxDecoration(
+            color: AppColors.primarySecondaryElementText,
+            borderRadius: BorderRadius.all(Radius.circular(5.w)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1))
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Logout",
+              style: TextStyle(
+                  color: AppColors.primaryElementText,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.normal),
+            )
+          ],
+        ),
+      ),
+      onTap: () {
+        Get.defaultDialog(
+            title: "Are you sure to logout?",
+            backgroundColor: Colors.white,
+            content: Container(),
+            radius: 5.0,
+            confirmTextColor: Colors.white,
+            onConfirm: () {},
+            onCancel: () {},
+            textConfirm: "Confirm",
+            textCancel: "Cancel");
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(child: Text("..Text.."),),
-      
+      appBar: _buildAppBar(),
+      body: SafeArea(
+          child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildProfilePhoto(),
+                  _buildCompleteBtn(),
+                  _buildLogOutBtn()
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
