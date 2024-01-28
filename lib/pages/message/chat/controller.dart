12c45adb5a9cbeaf3ab38f9faa2aca5cfb798359
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-01-28 23:08:56 +0530

    Chat module and send image part front end method

--- lib/pages/message/chat/controller.dart
+++ lib/pages/message/chat/controller.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:chatty/common/apis/apis.dart';
 import 'package:chatty/common/entities/entities.dart';
 import 'package:chatty/common/routes/names.dart';
 import 'package:chatty/common/store/store.dart';
@@ -7,6 +10,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ChatController extends GetxController {
   ChatController();
@@ -22,6 +26,8 @@ class ChatController extends GetxController {
   final db = FirebaseFirestore.instance;
   var listener;
   var isLoadmore = true;
+  File? _photo;
+  final ImagePicker _picker = ImagePicker();
   ScrollController myScrollController = ScrollController();
 
   void goMore() {
@@ -107,12 +113,88 @@ class ChatController extends GetxController {
           myScrollController.position.maxScrollExtent) {
         if (isLoadmore) {
           state.isLoading.value = true;
+          // to stop unnecessary request to firebase
           isLoadmore = false;
           asyncLoadMoreData();
         }
       }
     });
   }
+
+  Future imgFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _photo = File(pickedFile.path);
+      uploadFile();
+    } else {
+      print("No image selected");
+    }
+  }
+
+  Future uploadFile() async {
+    var result = await ChatAPI.upload_img(file: _photo);
+    print(result.data);
+    if (result.code == 0) {
+      sendImageMessage(result.data!);
+    } else {
+      toastInfo(msg: "Sending Image Error");
+    }
+  }
+
+  
+  void sendImageMessage(String url) async {
+    
+   
+
+    //Created an object to send to firebase
+    final content = Msgcontent(
+        token: token,
+        content: url,
+        type: "image",
+        addtime: Timestamp.now());
+
+    db
+        .collection('message')
+        .doc(doc_id)
+        .collection('msglist')
+        .withConverter(
+            fromFirestore: Msgcontent.fromFirestore,
+            toFirestore: (Msgcontent msg, options) => msg.toFirestore())
+        .add(content)
+        .then((DocumentReference doc) {
+      print('..... base id is $doc_id, new img docid is ${doc.id}');
+      
+    });
+
+    //collection().get().docs.data()
+    var messageResult = await db
+        .collection('message')
+        .doc(doc_id)
+        .withConverter(
+            fromFirestore: Msg.fromFirestore,
+            toFirestore: (Msg msg, options) => msg.toFirestore())
+        .get();
+    // to know if we have any unread messages or calls
+    if (messageResult.data() != null) {
+      var item = messageResult.data()!;
+      int to_msg_num = item.to_msg_num == null ? 0 : item.to_msg_num!;
+      int from_msg_num = item.from_msg_num == null ? 0 : item.from_msg_num!;
+      if (item.from_token == token) {
+        from_msg_num = from_msg_num + 1;
+      } else {
+        to_msg_num = to_msg_num + 1;
+      }
+
+      await db.collection("message").doc(doc_id).update({
+        "to_msg_num": to_msg_num,
+        "from_msg_num": from_msg_num,
+        "last_msg": "[image]",
+        "last_time": Timestamp.now()
+      });
+    }
+  }
+
+
 
   void asyncLoadMoreData() async {
     final messages = await db
