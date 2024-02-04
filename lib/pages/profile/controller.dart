cf25c2b6340d7a476b62b28ec4edfb74fbf4ea4c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-04 09:46:45 +0530

    Working on change avatar and functions

--- lib/pages/profile/controller.dart
+++ lib/pages/profile/controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chatty/common/apis/apis.dart';
 import 'package:chatty/common/entities/entities.dart';
 import 'package:chatty/common/store/store.dart';
@@ -6,6 +8,7 @@ import 'package:chatty/pages/profile/state.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileController extends GetxController {
   ProfileController();
@@ -13,6 +16,8 @@ class ProfileController extends GetxController {
   final state = ProfileState();
   TextEditingController nameController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  File? _photo;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void onInit() {
@@ -21,6 +26,13 @@ class ProfileController extends GetxController {
     if (userItem != null) {
       state.profile_detail.value = userItem;
     }
+  }
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    descriptionController.dispose();
+    super.onClose();
   }
 
   Future<void> goLogOut() async {
@@ -51,6 +63,38 @@ class ProfileController extends GetxController {
       UserItem userItem = state.profile_detail.value;
       await UserStore.to.saveProfile(userItem);
       Get.back(result: 'finished');
+    }
+  }
+
+  Future imgFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _photo = File(pickedFile.path);
+      //upload the file
+      uploadFile();
+    } else {
+      print("no image selected");
+    }
+  }
+
+  Future imgFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      _photo = File(pickedFile.path);
+      //upload the file
+      uploadFile();
+    } else {
+      print("no image selected");
+    }
+  }
+
+  Future uploadFile() async {
+    var result = await ChatAPI.upload_img(file: _photo);
+    if (result.code == 0) {
+      state.profile_detail.value.avatar = result.data;
+      state.profile_detail.refresh();
+    } else {
+      toastInfo(msg: "Image upload error");
     }
   }
 }
